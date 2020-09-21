547c8804da907e4f32f3f0c0b95aa15ec85c4f3a
Ivan Shvets <johnshvets@gmail.com>
2020-09-21 22:50:53 +0300

    change tasks 04 and 06

--- js/task-04.js
+++ js/task-04.js
@@ -3,9 +3,16 @@ const counterValue = document.querySelector('#value');
 const decrementBtn = document.querySelector('[data-action="decrement"]');
 const incrementBtn = document.querySelector('[data-action="increment"]');
 
-const increment = () =>
-  (counterValue.textContent = Number.parseInt(counterValue.textContent) + 1);
-const decrement = () => (counterValue.textContent -= 1);
+let counter = 0;
+
+const increment = () => {
+  counter += 1;
+  counterValue.textContent = counter;
+};
+const decrement = () => {
+  counter -= 1;
+  counterValue.textContent = counter;
+};
 
 decrementBtn.addEventListener('click', decrement);
 incrementBtn.addEventListener('click', increment);
--- js/task-06.js
+++ js/task-06.js
@@ -3,14 +3,15 @@ const validationLength = Number.parseInt(validationInputElement.dataset.length);
 
 const onInputChange = event => {
   const valueLength = event.currentTarget.value.length;
-  const currentClass = validationInputElement.className;
 
   if (valueLength !== validationLength) {
     validationInputElement.classList.remove('valid');
-    return validationInputElement.classList.add('invalid');
+    validationInputElement.classList.add('invalid');
   } else {
-    validationInputElement.classList.remove('invalid');
-    validationInputElement.classList.add('valid');
+    validationInputElement.classList.replace(
+      validationInputElement.className,
+      'valid',
+    );
   }
 };
 
